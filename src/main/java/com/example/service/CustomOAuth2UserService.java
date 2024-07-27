b27796f6c475bdee56d2f2f2b1896ab85f3cc7cc
textneckdeveloper <textneckdeveloper@gmail.com>
2024-07-28 01:57:41 +0900

    Add Modify OAuth 2.0 Session

--- src/main/java/com/example/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/service/CustomOAuth2UserService.java
@@ -26,6 +26,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService{
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
 		
 		OAuth2User oAuth2User = super.loadUser(userRequest);
+		
 		System.out.println(oAuth2User.getAttributes());
 		
 		String registrationId = userRequest.getClientRegistration().getRegistrationId();
